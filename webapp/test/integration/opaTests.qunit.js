512e9b17355612f249717c4b5354c8131875ccf2
Aroa Rodriguez <aroa.rodriguez@udefine.net>
2026-03-25 12:30:22 +0000

    primer envio de mi proyecto eCommerce

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["ecommerceproject/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"ecommerceproject/test/unit/controller/Home.controller"
+], function () {
+	"use strict";
+});
